a8cc5761f4583c6dff35d81f116038840921394a
Poxep <poxeprp@mail.ru>
2019-11-01 19:53:11 +0300

    пишем первую функцию

--- index.php
+++ index.php
@@ -134,7 +134,7 @@ function price_formatting($num) {
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?= $card_element['price']; ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?= price_formatting($card_element['price']); ?></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
